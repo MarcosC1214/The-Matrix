857b6e4eba2b80830ec11718375f505e5e132be6
Marcos Carrillo <83974878+0373067@users.noreply.github.com>
2021-05-12 20:55:16 -0400

    Update Main.java

--- TheMatrix/src/Matrix/Main.java
+++ TheMatrix/src/Matrix/Main.java
@@ -10,10 +10,10 @@ import javax.swing.SwingUtilities;
 
 import resources.SoundEffects;
 
-public class Main {
+public class Main { // Main method to boot up the program. 
 
 	public static SoundEffects sound = new SoundEffects();
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception { // Method to run the program along with the sound clip.
 		 sound.Run("Matrix.wav");
 		 SoundEffects.clip1.loop(Clip.LOOP_CONTINUOUSLY);
         // Swing GUIs should be created and altered on the EDT.
